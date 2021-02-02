2a0fbca45529d052a518f271f6e44ab0be422cca
Ryan Min <archkt@berkeley.edu>
2021-02-02 14:20:43 -0800

    1/3 heuristic: return number of corners left

--- searchAgents.py
+++ searchAgents.py
@@ -415,7 +415,10 @@ def cornersHeuristic(state, problem):
     walls = problem.walls # These are the walls of the maze, as a Grid (game.py)
 
     "*** YOUR CODE HERE ***"
-    return 0 # Default to trivial solution
+
+    return sum(i == False for i in state[1].values())
+
+    #return 0 # Default to trivial solution
 
 class AStarCornersAgent(SearchAgent):
     "A SearchAgent for FoodSearchProblem using A* and your foodHeuristic"
